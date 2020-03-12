2373de880f88a8feaf14ea91ec5cd254c1d3906c
loaki <jules.feuillatre@gmail.com>
2020-03-12 19:41:16 +0100

    test

---
+++ mini/minishell.h
@@ -0,0 +1,44 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "get_next_line.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+typedef struct	s_hist
+{
+	char			*line
+	struct s_hist	*next;
+}				t_cmd;
+
+typedef struct	s_cmd
+{
+	char 			*line;
+	char 			*ret;
+}				t_cmd;
+
+typedef struct	s_data
+{
+	t_hist 			*historic;
+//	int ret;
+}				t_data;
+
+//------------------------------ MINISHELL --------------------------------
+
+int			ft_start(t_struct *data);
+void		ft_print_prompt();
+
+void		ft_parse_line(t_struct *data);
+void		ft_treat(t_struct *data, char *line);
+
+//------------------------------- LIB ------------------------------------
+
+//	ft_init_lst(new);
+//	ft_init_data(&data);
+//	ft_lstadd_back(lst_cmd, new);
+//	ft_lstadd_front(lst_cmd, new);
+//	ft_split_shell(char *, char);
+
+//------------------------------------------------------------------------
+#endif
