fc2c9382a225588a14201f2d9c91340d434af88f
loaki <jules.feuillatre@gmail.com>
2020-03-12 19:22:32 +0100

    .h

---
+++ mini/minishell.h
@@ -0,0 +1,66 @@
+
+int			ft_start();
+void		ft_print_prompt();
+void		ft_parse_line(char *line);
+
+
+int		main(int ac, char **av)
+{
+	t_struct	data;
+
+	ft_init_data(&data);
+	ft_start(&data);
+	return (0);
+}
+
+int		ft_start(t_struct *data)
+{
+	char *line;
+
+	while(data->exit = 0)
+	{
+		ft_print_prompt();
+		get_next_line(0, &line);
+		ft_lstadd_front(data->historic, line);
+		ft_parse_line(&data);
+	}
+	return (0);
+}
+
+void		ft_print_prompt()
+{
+	write(1, "--->", 4);
+}
+
+void		ft_parse_line(t_struct *data)
+{
+	char	**cmd;
+	t_cmd	*lst_cmd;
+	
+	
+	ft_init_lst(lst_cmd);
+	cmd = ft_split_shell(data->historic->line, ';');
+	while (cmd[i])
+	{
+		ft_treat(cmd[i], lst_cmd);
+		i++;
+	}
+}
+
+void		ft_treat(t_struct *data, char *line)
+{
+	char 	**cmd;
+
+	cmd = ft_split_shell(line, '|');
+
+}	
+
+	
+
+//	ft_init_lst(new);
+//	ft_init_data(&data);
+//	ft_lstadd_back(lst_cmd, new);
+//	ft_lstadd_front(lst_cmd, new);
+//	ft_split_shell(char *, char);
+
+
