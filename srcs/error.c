f78b2fd7e459b8aaf939804278129c4a52c194da
acoudouy <acoudouy@student.42.fr>
2020-05-28 10:22:54 +0200

    ; | '' ok entre quote

--- srcs/error.c
+++ srcs/error.c
@@ -48,6 +48,25 @@ int			print_error_nl_expected()
 	return (1);
 }
 
+int			n_inside_q(char *s, int i)
+{
+	int		j;
+	int		k;
+	char	c;
+
+	j = 0;
+	while (s[j] && s[j] != '"' && s[j] != 39)
+		j++;
+	k = j;
+	if (s[j])
+		c = s[j];
+	while (s[k] && s[k] != c)
+		k++;
+	if (s[k] && j < i && k > i)
+		return (1);
+	return (0);
+}
+
 int			check_error_unexpected(char *s)
 {
 	int		i;
@@ -55,22 +74,20 @@ int			check_error_unexpected(char *s)
 	i = -1;
 	if (s[0] && (s[0] == '|' || s[0] == ';'))
 		return (print_error_unexpected(s[0]));
-//	if (s[0] && (s[0] == '<' || s[0] == '>'))
-//		return (print_error_nl_expected()); faut pas les mettre pour le > lol echo cou
 	while (s[++i])
 	{
 		if ((s[i] == '|' || s[i] == ';') && s[i + 1])
 		{
 			i++;
-			if (s[i] && (s[i] == '|' || s[i] == ';'))
+			if (s[i] && (s[i] == '|' || s[i] == ';') && n_inside_q(s, i) == 1)
 				return (print_error_unexpected(s[i]));
-			if (s[i] && (s[i] == '<' || s[i] == '>'))
+			if (s[i] && (s[i] == '<' || s[i] == '>') && n_inside_q(s, i) == 1)
 				return (print_error_nl_expected());
 			while (s[i] && s[i] == ' ')
 				i++;
-			if (s[i] && (s[i] == '|' || s[i] == ';'))
+			if (s[i] && (s[i] == '|' || s[i] == ';') && n_inside_q(s, i) == 1)
 				return (print_error_unexpected(s[i]));
-			if (s[i] && (s[i] == '<' || s[i] == '>'))
+			if (s[i] && (s[i] == '<' || s[i] == '>') && n_inside_q(s, i) == 1)
 				return (print_error_nl_expected());
 		}
 	}
